b70427bd151c7660248674217b13cd19b7559c07
Surya Gaddipati <surya.gaddipati@gmail.com>
2010-09-10 20:24:28 -0500

    intial

---
+++ src/clonic/core.clj
@@ -0,0 +1,1 @@
+(ns clonic.core)
---
+++ test/clonic/test/core.clj
@@ -0,0 +1,6 @@
+(ns clonic.test.core
+  (:use [clonic.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
